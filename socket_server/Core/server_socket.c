afd1e08be6f6d5d32320237fc1c98bc9fe85e42b
AshkanAbd <kotksthegame@gmail.com>
2020-10-22 22:22:37 +0330

    rename server side socket to server_socket.

--- socket_server/Core/server_socket.c
+++ socket_server/Core/server_socket.c
@@ -1,6 +1,6 @@
-#include "socket.h"
+#include "server_socket.h"
 
-int create_socket(struct Socket *s) {
+int create_socket(struct ServerSocket *s) {
     WSADATA Data;
     WSAStartup(MAKEWORD(2, 2), &Data);
 
@@ -12,13 +12,13 @@ int create_socket(struct Socket *s) {
     return 0;
 }
 
-void assign_port(struct Socket *s, int port) {
+void assign_port(struct ServerSocket *s, int port) {
     s->server_address.sin_family = AF_INET;
     s->server_address.sin_addr.s_addr = htonl(INADDR_ANY);
     s->server_address.sin_port = htons(port);
 }
 
-int bind_socket(struct Socket *s) {
+int bind_socket(struct ServerSocket *s) {
     if ((bind(s->socket_fd, (SA *) &(s->server_address), sizeof(s->server_address))) != 0) {
         return errno;
     } else {
@@ -26,7 +26,7 @@ int bind_socket(struct Socket *s) {
     }
 }
 
-int listen_socket(struct Socket *s) {
+int listen_socket(struct ServerSocket *s) {
     if ((listen(s->socket_fd, 5)) != 0) {
         return errno;
     }
@@ -34,7 +34,7 @@ int listen_socket(struct Socket *s) {
     return 0;
 }
 
-int accept_connection(struct Socket *s) {
+int accept_connection(struct ServerSocket *s) {
     s->connection_fd = accept(s->socket_fd, (SA *) &s->server_cli, &s->len);
     if (s->connection_fd < 0) {
         return errno;
@@ -42,7 +42,7 @@ int accept_connection(struct Socket *s) {
     return 0;
 }
 
-void close_socket(struct Socket *s) {
+void close_socket(struct ServerSocket *s) {
     close(s->socket_fd);
     WSACleanup();
 }
