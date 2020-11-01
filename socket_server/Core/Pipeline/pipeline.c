c5742a209f410039b59697b7900a2c7a25b0b8a8
AshkanAbd <kotksthegame@gmail.com>
2020-11-01 05:11:45 +0330

    create pipeline functions.

--- socket_server/Core/Pipeline/pipeline.c
+++ socket_server/Core/Pipeline/pipeline.c
@@ -1,8 +1,43 @@
 #include "pipeline.h"
 
 void init_pipeline(struct Pipeline *pipeline) {
+    pipeline->route_count = 0;
+
     pipeline->templates = malloc(1024 * sizeof(struct RouteTemplate));
     memset(pipeline->templates, 0, 1024 * sizeof(struct RouteTemplate));
 
     registerRoutes(pipeline->templates);
 }
+
+struct RouteTemplate *match_request(struct Pipeline *pipeline, struct IncomingRequest *request) {
+    struct RouteTemplate *template = NULL;
+    for (int i = 0; i < pipeline->route_count; ++i) {
+        struct RouteTemplate *tmp = pipeline->templates + i;
+        if (check_route(tmp, request)) {
+            if (check_action(tmp, request)) {
+                return tmp;
+            }
+            template = tmp;
+        }
+    }
+    return template;
+}
+
+int check_route(struct RouteTemplate *template, struct IncomingRequest *request) {
+    if (strcmp(template->route, request->route) == 0) {
+        return 1;
+    }
+    return 0;
+}
+
+int check_action(struct RouteTemplate *template, struct IncomingRequest *request) {
+    if (template->action == request->action) {
+        return 1;
+    }
+    return 0;
+}
+
+struct OutgoingResponse *execute_controller(struct IncomingRequest *request, struct RouteTemplate *template) {
+    return (*template->func)(request->payload);
+}
+
--- socket_server/Core/Pipeline/pipeline.h
+++ socket_server/Core/Pipeline/pipeline.h
@@ -3,11 +3,22 @@
 
 #include "../Router/router.h"
 #include "../Router/route_template.h"
+#include "../IO/incoming_request.h"
 
 struct Pipeline {
     struct RouteTemplate *templates;
+
+    int route_count;
 };
 
 void init_pipeline(struct Pipeline *pipeline);
+
+struct RouteTemplate *match_request(struct Pipeline *pipeline, struct IncomingRequest *request);
+
+struct OutgoingResponse *execute_controller(struct IncomingRequest *request, struct RouteTemplate *template);
+
+int check_route(struct RouteTemplate *template, struct IncomingRequest *request);
+
+int check_action(struct RouteTemplate *template, struct IncomingRequest *request);
 
 #endif //SOCKET_SERVER_PIPELINE_H
