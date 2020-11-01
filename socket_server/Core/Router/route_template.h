c2f05b5dc2402df79ff993211ff602910697b645
AshkanAbd <kotksthegame@gmail.com>
2020-11-01 04:07:35 +0330

    add router. add routerTemplate struct.

---
+++ socket_server/Core/Router/route_template.h
@@ -0,0 +1,9 @@
+#ifndef SOCKET_SERVER_ROUTE_TEMPLATE_H
+#define SOCKET_SERVER_ROUTE_TEMPLATE_H
+
+struct RouteTemplate {
+    int action;
+    char *route;
+};
+
+#endif //SOCKET_SERVER_ROUTE_TEMPLATE_H
---
+++ socket_server/Core/Router/router.h
@@ -0,0 +1,8 @@
+#ifndef SOCKET_SERVER_ROUTER_H
+#define SOCKET_SERVER_ROUTER_H
+
+#include "route_template.h"
+
+void registerRoutes(struct RouteTemplate *templates);
+
+#endif //SOCKET_SERVER_ROUTER_H
