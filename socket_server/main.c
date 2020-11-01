9f618dd3bd4b98ae0351be30294aba67636538ad
AshkanAbd <kotksthegame@gmail.com>
2020-11-02 00:23:37 +0330

    put all things together and connect pipeline to communicate with client.

--- socket_server/main.c
+++ socket_server/main.c
@@ -5,26 +5,32 @@
 #define MAX 1024
 #define PORT 8080
 
-void on_message_received(struct Client *c, char *msg);
-
 void on_client_connect(struct Client *c);
 
+void on_message_received(struct Client *c, char *msg);
+
 void on_client_disconnect(struct Client *c);
+
+void *handle_client(void *obj);
+
+void init_server(struct ServerSocket *server_socket);
 
 struct Client *clients;
 int clientIndex = 0;
 
-void init_server(struct ServerSocket *server_socket);
+struct Pipeline *pipeline;
 
 int main() {
     struct ServerSocket server_socket;
-    struct Pipeline pipeline;
 
     clients = malloc(MAX * sizeof(struct Client));
     memset(clients, 0, MAX * sizeof(struct Client));
 
+    pipeline = malloc(sizeof(struct Pipeline));
+    memset(pipeline, 0, sizeof(struct Pipeline));
+
     init_server(&server_socket);
-    init_pipeline(&pipeline);
+    init_pipeline(pipeline);
 
     while (1) {
         int connection = accept_connection(&server_socket);
@@ -35,10 +41,13 @@ int main() {
         } else {
             struct Client *client = (clients + clientIndex++);
             init_client(client, MAX, &connection);
-            set_message_func(client, on_message_received);
-            set_connect_func(client, on_client_connect);
-            set_disconnect_func(client, on_client_disconnect);
-            start_client(client);
+            pthread_t receive_thread;
+            pthread_create(&receive_thread, NULL, handle_client, client);
+
+//            set_connect_func(client, on_client_connect);
+//            set_message_func(client, on_message_received);
+//            set_disconnect_func(client, on_client_disconnect);
+//            start_client(client);
         }
     }
 }
@@ -71,11 +80,42 @@ void init_server(struct ServerSocket *server_socket) {
 
 }
 
-void on_message_received(struct Client *c, char *msg) {
+void *handle_client(void *obj) {
+    struct Client *client = (struct Client *) obj;
 
+    char *buffer = malloc(MAX);
+    memset(buffer, 0x1D, MAX);
+    recv(*client->socket, buffer, MAX, 0);
+
+    struct IncomingRequest request;
+    struct OutgoingResponse *response = malloc(sizeof(struct OutgoingResponse));
+    memset(response, 0, sizeof(struct OutgoingResponse));
+    if (!parse_request(&request, buffer, MAX)) {
+        init_invalid_syntax(response, NULL, 0);
+    } else {
+        struct RouteTemplate *routeTemplate = match_request(pipeline, &request);
+        if (routeTemplate == NULL) {
+            init_not_found(response, NULL, 0);
+        } else if (routeTemplate->action != request.action) {
+            init_invalid_action(response, NULL, 0);
+        }
+        memmove(response, execute_controller(&request, routeTemplate), sizeof(struct OutgoingResponse));
+    }
+
+    buffer = malloc(response->data_size + 2);
+    memset(buffer, 0x1D, response->data_size + 2);
+
+    *buffer = (char) response->status;
+    memcpy(buffer + 2, response->data, response->data_size);
+
+    send(*client->socket, buffer, response->data_size + 2, 0);
 }
 
 void on_client_connect(struct Client *c) {
+
+}
+
+void on_message_received(struct Client *c, char *msg) {
 
 }
 
