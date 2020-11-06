5c6be89032e736a37a7f1c827dbb098b503ba8f2
AshkanAbd <kotksthegame@gmail.com>
2020-11-06 16:29:59 +0330

    implement base64 encoder/decoder in server-side.

---
+++ socket_server/Core/base64/base64.h
@@ -0,0 +1,11 @@
+#ifndef SOCKET_SERVER_BASE64_H
+#define SOCKET_SERVER_BASE64_H
+
+#include <stdlib.h>
+#include <string.h>
+
+char *base64_encode(char *input_buffer);
+
+char *base64_decode(char *input_buffer);
+
+#endif //SOCKET_SERVER_BASE64_H
